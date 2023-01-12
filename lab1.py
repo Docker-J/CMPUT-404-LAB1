1d38f89eeac170f936a8b5c09d2d94920a3dc3f8
Junesung Lee <junesoung412@gmail.com>
2023-01-11 22:51:52 -0700

    GET and print itself from GitHub

--- lab1.py
+++ lab1.py
@@ -1,5 +1,8 @@
 import requests
 
 #print(requests.__version__)
-r = requests.get("http://www.google.com")
-print(r.status_code)
+#r = requests.get("http://www.google.com")
+#print(r.status_code)
+
+r = requests.get("https://raw.githubusercontent.com/Docker-J/CMPUT-404-LAB1/main/lab1.py")
+print(r.text)
